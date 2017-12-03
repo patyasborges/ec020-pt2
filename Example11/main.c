bb92fffa98d604388245e913ba27a76684cdf94b
patyasborges <patricia.adriane.sb@gmail.com>
2017-12-02 22:17:03 -0200

    funcionando

--- Example11/main.c
+++ Example11/main.c
@@ -131,26 +131,31 @@ void initAll() {
 	TCPLocalPort = TCP_PORT_HTTP;               // set port we want to listen to
 }
 
-int readAcc() {
+xData1 readAcc() {
+
+	xData1 total1;
+
 	int8_t x = 0;
 	int8_t y = 0;
 	int8_t z = 0;
 
 	acc_read(&x, &y, &z);
 
-	total.xoff = 0 - x;
-	total.yoff = 0 - y;
-	total.zoff = 64 - z;
+	total1.xoff = 0 - x;
+	total1.yoff = 0 - y;
+	total1.zoff = 64 - z;
+
+	return total1;
 }
 
-void printOled() {
+void printOled(xData1 t) {
 	char NewKey[6];
 
-	sprintf(NewKey, "%04d", total.xoff); // insert pseudo-ADconverter value
+	sprintf(NewKey, "%04d", t.xoff); // insert pseudo-ADconverter value
 	oled_putString(1, 25, NewKey, OLED_COLOR_BLACK, OLED_COLOR_WHITE);
-	sprintf(NewKey, "%04d", total.yoff); // insert pseudo-ADconverter value
+	sprintf(NewKey, "%04d", t.yoff); // insert pseudo-ADconverter value
 	oled_putString(1, 33, NewKey, OLED_COLOR_BLACK, OLED_COLOR_WHITE);
-	sprintf(NewKey, "%04d", total.zoff); // insert pseudo-ADconverter value
+	sprintf(NewKey, "%04d", t.zoff); // insert pseudo-ADconverter value
 	oled_putString(1, 41, NewKey, OLED_COLOR_BLACK, OLED_COLOR_WHITE);
 }
 
@@ -189,9 +194,9 @@ void vTaskFunction(void *pvParameters) {
 
 		/* Delay for a period. */
 		for (ul = 0; ul < mainDELAY_LOOP_COUNT1; ul++) {
-		/* This loop is just a very crude delay implementation.  There is
-		 nothing to do in here.  Later exercises will replace this crude
-		 loop with a proper delay/sleep function. */
+			/* This loop is just a very crude delay implementation.  There is
+			 nothing to do in here.  Later exercises will replace this crude
+			 loop with a proper delay/sleep function. */
 		}
 	}
 }
@@ -211,15 +216,15 @@ void vTaskFunction1(void *pvParameters) {
 
 		//readAcc();
 
-		printOled();
+		//printOled();
 
 		//printHttp();
 
 		/* Delay for a period. */
 		for (ul = 0; ul < mainDELAY_LOOP_COUNT1; ul++) {
-		/* This loop is just a very crude delay implementation.  There is
-		 nothing to do in here.  Later exercises will replace this crude
-		 loop with a proper delay/sleep function. */
+			/* This loop is just a very crude delay implementation.  There is
+			 nothing to do in here.  Later exercises will replace this crude
+			 loop with a proper delay/sleep function. */
 		}
 	}
 }
@@ -230,36 +235,34 @@ int main(void) {
 
 	initAll();
 
-	 /* The queue is created to hold a maximum of 5 long values. */
-	    xQueue = xQueueCreate( 5, sizeof( long ) );
+	/* The queue is created to hold a maximum of 5 long values. */
+	xQueue = xQueueCreate(3, sizeof( xData1 ));
 
-		if( xQueue != NULL )
-		{
-			/* Create two instances of the task that will write to the queue.  The
-			parameter is used to pass the value that the task should write to the queue,
-			so one task will continuously write 100 to the queue while the other task
-			will continuously write 200 to the queue.  Both tasks are created at
-			priority 1. */
-			//xTaskCreate( vSenderTask, "Sender1", 240, ( void * ) 100, 1, NULL );
-			xTaskCreate( vSenderTask, "Sender2", 240, ( void * ) 200, 1, NULL );
+	if (xQueue != NULL ) {
+		/* Create two instances of the task that will write to the queue.  The
+		 parameter is used to pass the value that the task should write to the queue,
+		 so one task will continuously write 100 to the queue while the other task
+		 will continuously write 200 to the queue.  Both tasks are created at
+		 priority 1. */
+		//xTaskCreate( vSenderTask, "Sender1", 240, ( void * ) 100, 1, NULL );
+		xTaskCreate(vSenderTask, "Sender2", 240, (void * ) 200, 1, NULL);
 
-			/* Create the task that will read from the queue.  The task is created with
-			priority 2, so above the priority of the sender tasks. */
-			xTaskCreate( vReceiverTask, "Receiver", 240, NULL, 2, NULL );
+		/* Create the task that will read from the queue.  The task is created with
+		 priority 2, so above the priority of the sender tasks. */
+		xTaskCreate(vReceiverTask, "Receiver", 240, NULL, 2, NULL);
 
-			/* Start the scheduler so the created tasks start executing. */
-			vTaskStartScheduler();
-		}
-		else
-		{
-			/* The queue could not be created. */
-		}
+		/* Start the scheduler so the created tasks start executing. */
+		vTaskStartScheduler();
+	} else {
+		/* The queue could not be created. */
+	}
 
-	    /* If all is well we will never reach here as the scheduler will now be
-	    running the tasks.  If we do reach here then it is likely that there was
-	    insufficient heap memory available for a resource to be created. */
-		for( ;; );
-		return 0;
+	/* If all is well we will never reach here as the scheduler will now be
+	 running the tasks.  If we do reach here then it is likely that there was
+	 insufficient heap memory available for a resource to be created. */
+	for (;;)
+		;
+	return 0;
 
 	//xQueue = xQueueCreate(3, sizeof(xData));
 
@@ -419,108 +422,77 @@ void InsertDynamicValues(void) {
  }*/
 /*-----------------------------------------------------------*/
 
-static void vSenderTask( void *pvParameters )
-{
-long lValueToSend;
-portBASE_TYPE xStatus;
+static void vSenderTask(void *pvParameters) {
+	xData1 lValueToSend;
+	portBASE_TYPE xStatus;
 
-	/* Two instances are created of this task so the value that is sent to the
-	queue is passed in via the task parameter rather than be hard coded.  This way
-	each instance can use a different value.  Cast the parameter to the required
-	type. */
-	lValueToSend = ( long ) pvParameters;
+	//lValueToSend = ( long ) pvParameters;
 
 	/* As per most tasks, this task is implemented within an infinite loop. */
-	for( ;; )
-	{
-		/* The first parameter is the queue to which data is being sent.  The
-		queue was created before the scheduler was started, so before this task
-		started to execute.
+	for (;;) {
 
-		The second parameter is the address of the data to be sent.
+		lValueToSend = readAcc();
 
-		The third parameter is the Block time � the time the task should be kept
-		in the Blocked state to wait for space to become available on the queue
-		should the queue already be full.  In this case we don�t specify a block
-		time because there should always be space in the queue. */
 		xStatus = xQueueSendToBack( xQueue, &lValueToSend, 0 );
 
-		if( xStatus != pdPASS )
-		{
+		if (xStatus != pdPASS) {
 			/* We could not write to the queue because it was full � this must
-			be an error as the queue should never contain more than one item! */
-			vPrintString( "Could not send to the queue.\r\n" );
+			 be an error as the queue should never contain more than one item! */
+			vPrintString("Could not send to the queue.\r\n");
 		}
 
-		readAcc();
-
-			//printOled();
-
-			//printHttp();
-
-		/* Allow the other sender task to execute. */
 		taskYIELD();
 	}
 }
 /*-----------------------------------------------------------*/
 
-static void vReceiverTask( void *pvParameters )
-{
-/* Declare the variable that will hold the values received from the queue. */
-long lReceivedValue;
-portBASE_TYPE xStatus;
-const portTickType xTicksToWait = 100 / portTICK_RATE_MS;
+static void vReceiverTask(void *pvParameters) {
+	/* Declare the variable that will hold the values received from the queue. */
+	xData1 lReceivedValue;
+	portBASE_TYPE xStatus;
+	const portTickType xTicksToWait = 100 / portTICK_RATE_MS;
 
 	/* This task is also defined within an infinite loop. */
-	for( ;; )
-	{
+	for (;;) {
 		/* As this task unblocks immediately that data is written to the queue this
-		call should always find the queue empty. */
-		if( uxQueueMessagesWaiting( xQueue ) != 0 )
-		{
-			vPrintString( "Queue should have been empty!\r\n" );
+		 call should always find the queue empty. */
+		if (uxQueueMessagesWaiting(xQueue) != 0) {
+			vPrintString("Queue should have been empty!\r\n");
 		}
-
 
 		xStatus = xQueueReceive( xQueue, &lReceivedValue, xTicksToWait );
 
 		//printHttp();
 
-		if( xStatus == pdPASS )
-		{
+		if (xStatus == pdPASS) {
 			/* Data was successfully received from the queue, print out the received
-			value. */
-			vPrintStringAndNumber( "Received = ", lReceivedValue );
-			printOled();
-		}
-		else
-		{
+			 value. */
+			vPrintStringAndNumber("Received = ", lReceivedValue.xoff);
+			printOled(lReceivedValue);
+		} else {
 			/* We did not receive anything from the queue even after waiting for 100ms.
-			This must be an error as the sending tasks are free running and will be
-			continuously writing to the queue. */
-			vPrintString( "Could not receive from the queue.\r\n" );
+			 This must be an error as the sending tasks are free running and will be
+			 continuously writing to the queue. */
+			vPrintString("Could not receive from the queue.\r\n");
 		}
 	}
 }
 
-void vApplicationMallocFailedHook( void )
-{
+void vApplicationMallocFailedHook(void) {
 	/* This function will only be called if an API call to create a task, queue
-	or semaphore fails because there is too little heap RAM remaining. */
-	for( ;; )
+	 or semaphore fails because there is too little heap RAM remaining. */
+	for (;;)
 		;
 }
 
 /*-----------------------------------------------------------*/
 
-void vApplicationIdleHook( void )
-{
+void vApplicationIdleHook(void) {
 	/* This example does not use the idle hook to perform any processing. */
 }
 /*-----------------------------------------------------------*/
 
-void vApplicationTickHook( void )
-{
+void vApplicationTickHook(void) {
 	/* This example does not use the tick hook to perform any processing. */
 }
 /*-----------------------------------------------------------*/
